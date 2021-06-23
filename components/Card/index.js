df348942c9e6c7f0cfecd629b3060d1fbe6ea072
SaraSaivetto <sara.saivetto@ied.edu>
2021-06-23 12:42:54 +0200

    test

---
+++ components/Card/index.js
@@ -0,0 +1,7 @@
+export default function Card(TITOLO, TESTO) {
+    return `
+        <div>
+            <h1>${TITOLO}</h1>
+            <p>${TESTO}</p>
+        </div>`
+}
--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-import Hello from './components/Hello';
-import Title from './components/Title';
-import Draw from './components/Draw';
+// import Hello from './components/Hello';
+// import Title from './components/Title';
+// import Draw from './components/Draw';
+
+import Card from './components/Card';
 
 function app() {
   document.body.innerHTML = `
   <main>
-  ${Title('Questo è un titolo')}
-  <br/>
-  ${Hello}
-  <br/>
-  ${Draw}
+    <article>
+    ${Card("TITOLO", "TESTO")}
+    ${Card("TITOLO", "TESTO")}
+    </article>
   <main>`;
 }
 
